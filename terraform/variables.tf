7b73c7533f71b8926dde0a9459ab90d157694c2d
maheshvara75 <maheshvara4gmai@ip-172-31-26-244.ec2.internal>
2024-06-20 02:00:58 +0000

    updated ansible and terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_access_key" {
+  description = "AWS_ACCESS_KEY"
+  default =""
+  }
+variable "aws_secret_key" {
+  description = "AWS_SECRET_KEY"
+  default =""
+  }
+variable "aws_session_token" {
+  description = "AWS_ACCESS_TOKEN"
+  default=""
+  }
+variable "key_name" {
+  description = "AWS_KEY_NAME"
+  default =""
+  }
+variable "instance_ami" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-04b70fa74e45c3917"
+}
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
